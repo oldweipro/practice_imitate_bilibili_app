7e15fca14be7d15d804ec6eff3130f98af2af686
oldwei <oldweipro@gmail.com>
2021-06-23 00:52:19 +0800

    修复添加header参数结果为空

--- lib/http/request/base_request.dart
+++ lib/http/request/base_request.dart
@@ -1,15 +1,21 @@
-enum HttpMethod { GET, POST, DELETE }
+enum HttpMethod { GET, POST, PUT, DELETE }
 //基础请求
 
 abstract class BaseRequest {
+  //请求路由参数/{param}
   var pathParams;
+  //是否启用https，默认启用https
   var useHttps = true;
+  //设置请求域名
   String authority() {
     return "api.devio.org";
   }
 
+  //请求方法
   HttpMethod httpMethod();
+  //请求路由
   String path();
+  //生成具体的URL
   String url() {
     Uri uri;
     var pathStr = path();
@@ -31,17 +37,21 @@ abstract class BaseRequest {
     return uri.toString();
   }
 
+  //设置接口是否需要登录
   bool needLogin();
+  //请求参数封装
   Map<String, String> params = Map();
-
+  //添加请求参数方法
   BaseRequest add(String k, Object v) {
     params[k] = v.toString();
     return this;
   }
 
+  //添加headers参数
   Map<String, dynamic> header = Map();
+  //添加参数方法
   BaseRequest addHeader(String k, Object v) {
-    params[k] = v.toString();
+    header[k] = v.toString();
     return this;
   }
 }
